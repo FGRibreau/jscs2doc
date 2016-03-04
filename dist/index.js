d04390c181805a02edb6ea6085fc1c05711927fb
FG Ribreau <github@fgribreau.com>
2016-03-04 15:01:10 +0100

    fix(index): fix require path

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,4 @@
 'use strict';
 
 // @todo create a real commande line
-require('./jscs2doc')(require('path').resolve(__dirname, '..', process.argv[2]), require('path').resolve(__dirname, '..', process.argv[3]));
+require('./jscs2doc')(require('path').resolve(process.cwd(), process.argv[2]), require('path').resolve(process.cwd(), process.argv[3]));
--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
 // @todo create a real commande line
-require('./jscs2doc')(require('path').resolve(__dirname, '..', process.argv[2]), require('path').resolve(__dirname, '..', process.argv[3]));
+require('./jscs2doc')(require('path').resolve(process.cwd(), process.argv[2]), require('path').resolve(process.cwd(), process.argv[3]));
